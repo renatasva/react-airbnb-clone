f2ae5bf166176ea521ff9c11ec3c1bb7cd48fcf4
Renata Svaboviciute <renata.svaboviciute@gmail.com>
2019-01-07 18:15:55 +0000

    adding flexbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Flat from './components/flat';
 
 class App extends Component {
   render() {
+
+    const flat = {
+      "name": "Trendy Apt in Buttes Montmartre",
+      "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
+      "price": 200,
+      "priceCurrency": "EUR",
+      "lat": 48.885707,
+      "lng": 2.343543
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className="main">
+          <div className="search">
+          </div>
+          <div className="flats">
+            <Flat flat={flat} />
+            <Flat flat={flat} />
+          </div>
+        </div>
+        <div className="map">
+        </div>
       </div>
     );
   }
