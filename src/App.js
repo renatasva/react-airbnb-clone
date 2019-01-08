80dd0efb9373f59b2a5c385e6cf1b3561093cf81
Renata Svaboviciute <renata.svaboviciute@gmail.com>
2019-01-08 14:32:15 +0000

    added map and markers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 // import logo from './logo.svg';
 import './App.css';
 import Flat from './components/flat';
+import Marker from './components/marker';
 
 class App extends Component {
   constructor(props) {
@@ -20,8 +22,14 @@ class App extends Component {
         flats: data
       })
     })
-  }
+  };
+
   render() {
+    const center = {
+      lat: 48.8566,
+      lng: 2.3522
+    }
+
     return (
       <div className="app">
         <div className="main">
@@ -34,6 +42,14 @@ class App extends Component {
           </div>
         </div>
         <div className="map">
+        <GoogleMapReact
+          center={center}
+          zoom={11}
+        >
+          {this.state.flats.map((flat) => {
+            return <Marker lat={flat.lat} lng={flat.lng} text={flat.price} />
+          })}
+        </GoogleMapReact>
         </div>
       </div>
     );
