b49803c715b6e46f3b3922f7f34cdbfe018a1d19
Renata Svaboviciute <renata.svaboviciute@gmail.com>
2019-01-08 10:59:11 +0000

    fetched data from api

--- src/App.js
+++ src/App.js
@@ -4,25 +4,33 @@ import './App.css';
 import Flat from './components/flat';
 
 class App extends Component {
-  render() {
-
-    const flat = {
-      "name": "Trendy Apt in Buttes Montmartre",
-      "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
-      "price": 200,
-      "priceCurrency": "EUR",
-      "lat": 48.885707,
-      "lng": 2.343543
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
     };
+  }
 
+  componentDidMount(){
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      this.setState({
+        flats: data
+      })
+    })
+  }
+  render() {
     return (
       <div className="app">
         <div className="main">
           <div className="search">
           </div>
           <div className="flats">
-            <Flat flat={flat} />
-            <Flat flat={flat} />
+            {this.state.flats.map((flat) => {
+              return <Flat flat={flat} />
+            })}
           </div>
         </div>
         <div className="map">
